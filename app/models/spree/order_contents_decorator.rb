5ce6457f7cdd9d02502a0641df4068ed9b8d567b
Alessandro Lepore <lepore.ale@gmail.com>
2015-05-05 14:40:57 +0200

    force line item price list (?)

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -3,16 +3,17 @@ module Spree
     private
 
     def add_to_line_item(variant, quantity, options = {})
-      price_list = options[:price_list]
       line_item = grab_line_item_by_variant(variant, false, options)
+      price_list = order.price_list
 
       if line_item
         line_item.quantity += quantity.to_i
-        line_item.currency = price_list.currency if price_list
+        line_item.currency = order.currency
         line_item.price_list = price_list
       else
-        opts = { currency: order.currency,
-                 price_list: price_list}.merge ActionController::Parameters.new(options).
+        base_opts = { currency: order.currency,
+                      price_list: price_list }
+        opts = base_opts.merge ActionController::Parameters.new(options).
                                          permit(PermittedAttributes.line_item_attributes)
         line_item = order.line_items.new(quantity: quantity,
                                           variant: variant,
