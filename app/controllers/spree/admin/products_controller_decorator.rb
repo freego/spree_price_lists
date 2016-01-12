771ec3c1726a4f16417a6f63122e868ee6f8c072
Alessio Rocco <alessio.rocco.lt@gmail.com>
2016-01-12 14:36:06 +0100

    add price list fields to product admin form

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,10 +1,20 @@
-module Spree
-  module Admin
-    ProductsController.class_eval do
-      def price_lists
-        @product = Product.friendly.find(params[:product_id])
-        @price_lists = PriceList.order(:name)
-      end
+Spree::Admin::ProductsController.class_eval do
+  before_action :build_prices, only: [:edit]
+
+  def price_lists
+    @product = Spree::Product.friendly.find(params[:product_id])
+    @price_lists = Spree::PriceList.order(:name)
+  end
+
+  private
+
+  def build_prices
+    Spree::PriceList.where.not(id: price_list_ids).each do |pl|
+      @object.master.prices.build(price_list: pl) unless pl.default?
     end
+  end
+
+  def price_list_ids
+    @object.master.prices.pluck(:price_list_id)
   end
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,6 +2,8 @@ module Spree
   Product.class_eval do
     delegate_belongs_to :master, :price_for
 
+    accepts_nested_attributes_for :master
+
     def variants_and_option_values_for_price_list(current_price_list = nil)
       variants.includes(:option_values)
               .active_for_price_list(current_price_list)
